6a7081112359d81e2e38516d5b2956b297e20a0a
tanbirk <tanvir.kashyap879@gmail.com>
2021-12-01 15:42:14 +0530

    fin

---
+++ components/Thumbnail.js
@@ -0,0 +1,9 @@
+function Thumbnail({ result }) {
+    return (
+        <div>
+            <h1>GAME</h1>
+        </div>
+    )
+}
+
+export default Thumbnail
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
+import { Result } from 'postcss'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({ results }) {
+ 
   return (
     <div>
       <Head>
@@ -15,7 +19,20 @@ export default function Home() {
       {/* nav component */}
        <Nav />  
         {/* result component */}
-
+      <Results results={results} />
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const genre =context.query.genre;
+
+  const request =await fetch(`https://api.themoviedb.org/3${requests[genre]?.url ||
+  requests.fetchTrending.url
+  }`).then(res => res.json())
+  return {
+    props:{
+      results: request.results
+    }
+  }
 }
